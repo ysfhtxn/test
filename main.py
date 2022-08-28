46c803e9ec9cef72e1ae0314495a2ce0e145d378
ysfhtxn <1121656712@qq.com>
2022-08-28 22:32:52 +0800

    .

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
 file = open('test.txt','w',encoding='utf-8')
 file.write('test')
 file.close()
+os.system('cd /d F:\/OneDrive\/Documents\/blog\/ && npm install hexo-deployer-git --save && hexo clean && hexo g && hexo d')
