3746f06a693c622a773bf8ce34a4729be2fe2e41
ysfhtxn <52019664+ysfhtxn@users.noreply.github.com>
2022-08-29 23:50:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,3 @@ import sys
 file = open('test.txt','w',encoding='utf-8')
 file.write('test')
 file.close()
-os.system('cd /d F:\/OneDrive\/Documents\/blog\/ && npm install hexo-deployer-git --save && hexo clean && hexo g && hexo d')
