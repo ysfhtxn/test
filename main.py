d7f7b7faeae780f7abdef61f289a26b1ca77b9d6
ysfhtxn <1121656712@qq.com>
2022-10-07 22:42:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import os
 import sys
 
-god = 'test'
+god = '---\n'
+god = god + 'title: 今日更新番剧\n'
+god = god + 'date: 2022-9-29 12:00:00\n'
+god = god + 'tags: anime\n'
+god = god + '---\n'
+
+god = god + '1'
+god = god + '2'
+god = god + '3'
+god = god + '4'
+
 
 print(god)
